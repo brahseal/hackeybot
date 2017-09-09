f0de8836e9b2e9dfd66a0140527e276114869a1d
customer <customer@s98992.nocix.net>
2017-09-08 19:36:39 -0500

    fixed import syntax

---
+++ reddit.auth.py
@@ -0,0 +1,7 @@
+import praw
+
+reddit = praw.Reddit(client_id='T7kHvMJtr3phzA',
+                     client_secret='U_7Hss4zYlCF_sv5sMgyahvsqIg',
+                     password='n3zx28!GKnZNNR4',
+                     user_agent='<macOS>:<1>:<0> (by /u/<hackeybotthrowaway>)',
+                     username='SafeStreamsBot')
