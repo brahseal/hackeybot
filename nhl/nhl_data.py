52a8756a17bbdccebf7fae664fe116688bbe563f
customer <customer@s98992.nocix.net>
2017-09-08 20:09:18 -0500

    added goalie support

--- nhl/nhl_data.py
+++ nhl/nhl_data.py
@@ -6,12 +6,23 @@ from sys import exit
 import json
 import csv
 
-url = 'http://www.nhl.com/stats/rest/grouped/skaters/basic/season/bios?cayenneExp=gameTypeId=%222%22%20and%20seasonId%3E=20162017%20and%20seasonId%3C=20162017&factCayenneExp=gamesPlayed%3E=60&sort=[{%22property%22:%22playerBirthDate%22,%22direction%22:%22DESC%22}]'
+players = 'http://www.nhl.com/stats/rest/grouped/skaters/basic/season/bios?cayenneExp=gameTypeId=%222%22%20and%20seasonId%3E=20162017%20and%20seasonId%3C=20162017&factCayenneExp=gamesPlayed%3E=60&sort=[{%22property%22:%22playerBirthDate%22,%22direction%22:%22DESC%22}]'
 
-resp = requests.get(url).text
-resp = json.loads(resp)
+goalies = 'http://www.nhl.com/stats/rest/grouped/goalies/goalie_basic/season/goaliesummary?cayenneExp=gameTypeId=%222%22%20and%20playerPositionCode=%22G%22%20and%20seasonId%3E=20162017%20and%20seasonId%3C=20162017&factCayenneExp=gamesPlayed%3E=1&sort=[{%22property%22:%22wins%22,%22direction%22:%22DESC%22}]'
+
+respPlayers = requests.get(players).text
+respPlayers = json.loads(respPlayers)
+
+respGoalies = requests.get(goalies).text
+respGoalies = json.loads(respGoalies)
 
 def get_mugshot(player_name):
-	for x in range(0, len(resp['data'])):
-		if resp['data'][x]['playerLastName'].lower() == player_name:
-			return('https://nhl.bamcontent.com/images/headshots/current/168x168/' + str(resp['data'][x]['playerId']) + '.jpg')
+
+	for x in range(0, len(respPlayers['data'])):
+		if respPlayers['data'][x]['playerLastName'].lower() == player_name:
+			return('https://nhl.bamcontent.com/images/headshots/current/168x168/' + str(respPlayers['data'][x]['playerId']) + '.jpg')
+	
+	
+	for x in range(0, len(respGoalies['data'])):
+		if respGoalies['data'][x]['playerLastName'].lower() == player_name:
+			return('https://nhl.bamcontent.com/images/headshots/current/168x168/' + str(respGoalies['data'][x]['playerId']) + '.jpg')
